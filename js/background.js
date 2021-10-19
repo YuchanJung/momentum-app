1f6eea1b78a1df1f6ac958163cdd9fcb0af5b3e9
yuchannn <yuchanbpp@gmail.com>
2021-10-19 20:51:54 +0900

    change background randomly

---
+++ js/background.js
@@ -0,0 +1,10 @@
+const images = [
+    "0.webp",
+    "1.webp",
+    "2.webp"
+];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgimage = document.createElement("img");
+bgimage.src = `img/${chosenImage}`;
+document.body.appendChild(bgimage);
